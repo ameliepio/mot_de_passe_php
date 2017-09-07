05489d82e3ec3bc6b827bdbcd6e1c79239f00f96
Amelie <a.pioger@outlook.fr>
2017-09-07 11:08:47 +0200

    second commit

--- formulaire.php
+++ formulaire.php
@@ -1,10 +1,31 @@
 
 
-<form action="secret.php" method="post"enctype="multipart/form-data">
+<!-- <form action="secret.php" method="post"enctype="multipart/form-data">
 
 <p>
 <input type="password"name="mdp"/><br>
 
 <input type="submit" value="mot de passe"/>
 </p>
-</form>
+</form> -->
+
+<?php
+if (isset($_POST["mdp"]) AND $_POST["mdp"] == "kangourou"){
+
+ echo "kangourou";
+
+ }else{
+
+  ?>
+
+   <form action="formulaire.php" method="post" enctype="multipart/form-data">
+
+   <p>
+   <input type="password" name="mdp"/><br>
+
+   <input type="submit" value="mot de passe"/>
+   </p>
+  </form>
+<?php
+}
+?>
--- secret.php
+++ secret.php
@@ -1,14 +1,14 @@
-<?php
+<!-- <?php
 
-var_dump($_POST["mdp"]);
 
-if (isset($_POST["mdp"]) AND $_POST["mdp"] == "kangourou"){
+//
+// if (isset($_POST["mdp"]) AND $_POST["mdp"] == "kangourou"){
+//
+// echo "le mot de passe est correct";
+//
+// }else {
+//
+// echo "mauvais mot de passe";
+// }
 
-echo "le mot de passe est correct";
-
-}else {
-
-echo "mauvais mot de passe";
-}
-
-?>
+?> -->
